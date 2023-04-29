ed62ec6c90fc0d5c8ea3fd5b5e0f492256e68141
Yevhen Arkhipov <arkhipovyevhen@gmail.com>
2023-04-29 12:37:32 +0300

    fix routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { lazy } from "react";
+import { useEffect, useState, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import Layout from "../Layout";
@@ -7,14 +7,26 @@ const Home = lazy(() => import("../../pages/Home"));
 const Tweets = lazy(() => import("../../pages/Tweets"));
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/tweets" element={<Tweets />} />
-      </Route>
-      <Route path="*" element={<Home />} />
-    </Routes>
+    <>
+      {loading ? (
+        <h1>Loading</h1>
+      ) : (
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/tweets" element={<Tweets />} />
+          </Route>
+          <Route path="*" element={<Home />} />
+        </Routes>
+      )}
+    </>
   );
 };
 
