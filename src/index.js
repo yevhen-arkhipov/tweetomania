3e27327c949c65abd875d42282ac4954c08a1f79
Yevhen Arkhipov <arkhipovyevhen@gmail.com>
2023-04-29 12:14:53 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/tweetomania">
       <App />
       <GlobalStyles />
     </BrowserRouter>
