c615ec95bccaac859aadd24076e152461c909538
Yevhen Arkhipov <arkhipovyevhen@gmail.com>
2023-04-29 10:59:30 +0300

    check

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
         <Route path="/tweets" element={<Tweets />} />
         <Route path="*" element={<Home />} />
       </Route>
+      <Route path="*" element={<Home />} />
     </Routes>
   );
 };
