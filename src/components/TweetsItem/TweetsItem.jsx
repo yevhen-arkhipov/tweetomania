9949a7cfe5bd1873acf3c1c209056f3be6442478
Yevhen Arkhipov <arkhipovyevhen@gmail.com>
2023-05-02 11:40:09 +0300

    fix and updates

--- src/components/TweetsItem/TweetsItem.jsx
+++ src/components/TweetsItem/TweetsItem.jsx
@@ -24,7 +24,7 @@ import {
   FollowButton,
 } from './TweetsItem.styled';
 
-const TweetsItem = ({ name, tweets, followers, avatar, filter }) => {
+const TweetsItem = ({ name, tweets, followers, avatar }) => {
   const [isHovering, setIsHovering] = useState(false);
   const [followersCount, setFollowersCount] = useState(followers);
   const [isClicked, setIsClicked] = useState(false);
@@ -66,33 +66,8 @@ const TweetsItem = ({ name, tweets, followers, avatar, filter }) => {
   const handleFollowClick = () => {
     setIsClicked(!isClicked);
     setFollowersCount(isClicked ? followersCount - 1 : followersCount + 1);
-    updateLocalStorage({ isClicked, followersCount, name, filter });
+    updateLocalStorage({ isClicked, followersCount, name });
   };
-
-  // const updateLocalStorage = () => {
-  //   localStorage.setItem(
-  //     name,
-  //     JSON.stringify({ isClicked: !isClicked, followersCount })
-  //   );
-
-  //   const savedFilter = localStorage.getItem('filter');
-
-  //   if (savedFilter && savedFilter !== filter) {
-  //     const savedData = JSON.parse(localStorage.getItem(savedFilter)) || [];
-  //     const updatedData = savedData.filter(item => item.name !== name);
-  //     localStorage.setItem(savedFilter, JSON.stringify(updatedData));
-  //   }
-
-  //   const currentFilter = localStorage.getItem('filter');
-  //   const isFollow = currentFilter === 'follow' && !isClicked;
-  //   const isFollowing = currentFilter === 'following' && isClicked;
-
-  //   if (isFollow || isFollowing) {
-  //     const savedData = JSON.parse(localStorage.getItem(currentFilter)) || [];
-  //     const updatedData = savedData.filter(item => item.name !== name);
-  //     localStorage.setItem(currentFilter, JSON.stringify(updatedData));
-  //   }
-  // };
 
   return (
     <StylesTweetsItem
--- src/utils/updateLocalStorage.js
+++ src/utils/updateLocalStorage.js
@@ -1,25 +1,29 @@
-const updateLocalStorage = ({ isClicked, followersCount, name, filter }) => {
+const updateLocalStorage = ({ isClicked, followersCount, name }) => {
   localStorage.setItem(
     name,
     JSON.stringify({ isClicked: !isClicked, followersCount })
   );
 
-  const savedFilter = localStorage.getItem('filter');
+  const savedFilter = localStorage.getItem(name);
 
-  if (savedFilter && savedFilter !== filter) {
-    const savedData = JSON.parse(localStorage.getItem(savedFilter)) || [];
-    const updatedData = savedData.filter(item => item.name !== name);
-    localStorage.setItem(savedFilter, JSON.stringify(updatedData));
+  if (savedFilter && savedFilter !== name) {
+    const savedData = JSON.parse(localStorage.getItem(savedFilter)) || {};
+    if (Array.isArray(savedData)) {
+      const updatedData = savedData.filter(item => item.name !== name);
+      localStorage.setItem(savedFilter, JSON.stringify(updatedData));
+    }
   }
 
-  const currentFilter = localStorage.getItem('filter');
+  const currentFilter = localStorage.getItem(name);
   const isFollow = currentFilter === 'follow' && !isClicked;
   const isFollowing = currentFilter === 'following' && isClicked;
 
   if (isFollow || isFollowing) {
-    const savedData = JSON.parse(localStorage.getItem(currentFilter)) || [];
-    const updatedData = savedData.filter(item => item.name !== name);
-    localStorage.setItem(currentFilter, JSON.stringify(updatedData));
+    const savedData = JSON.parse(localStorage.getItem(currentFilter)) || {};
+    if (Array.isArray(savedData)) {
+      const updatedData = savedData.filter(item => item.name !== name);
+      localStorage.setItem(currentFilter, JSON.stringify(updatedData));
+    }
   }
 };
 
